193e283d1da54c11d307ec0ea76772d95a061d6b
Maat9090 <Maat9090@gmail.com>
2022-05-10 23:03:34 +0200

    First Commit testNG

---
+++ src/FirstTestCase.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+
+    @Test
+
+    void setup(){
+        System.out.println(" Opening browser. ");
+    }
+
+    @Test
+    void login(){
+        System.out.println("This is login test.");
+    }
+
+    @Test
+    void teardown(){
+        System.out.println("Closing browser.");
+
+    }
+
+
+
+
+
+}
